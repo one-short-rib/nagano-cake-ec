7a777bef6a8bf5d59ddd8f422ce9e1d3073f3e1e
hojokaban <hojokaban@gmail.com>
2020-05-14 11:11:51 +0000

    Add null:false to new explanation column
    null:false何度も挑戦するが、schemaに反映されず

--- db/migrate/20200514105135_add_explanation_to_item.rb
+++ db/migrate/20200514105135_add_explanation_to_item.rb
@@ -1,5 +1,5 @@
 class AddExplanationToItem < ActiveRecord::Migration[5.2]
   def change
-    add_column :items, :explanation, :string
+    add_column :items, :explanation, :string, null: false
   end
 end
