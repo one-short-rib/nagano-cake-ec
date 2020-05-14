e452e39651bfca0415004be480b5b337de4cc83f
hojokaban <hojokaban@gmail.com>
2020-05-14 09:45:15 +0000

    Add validations to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
+  has_many :order_items
+  has_many :cart_items
   belongs_to :genre
+
+  attachment :item_image
+
+  validates :genre_id, presence :true
+  validates :name, presence :true
+  validates :explanation, presence :true
+  validates :price, presence :true
+  validates :is_saled, presence :true
+  validates :item_image, presence :true
 end
