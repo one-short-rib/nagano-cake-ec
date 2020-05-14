ae9c9f62bcdcc9934463127a82f414243455ba8f
Hyouyou <mums.kh16.29@icloud.com>
2020-05-14 10:35:27 +0000

    add model

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,9 @@
 class Ship < ApplicationRecord
+
+	belongs_to :user
+
+    validates　:member_id, presence: true
+	validates　:postal_code, presence: true
+	validates　:address, presence: true
+	validates　:name, presence: true
 end
