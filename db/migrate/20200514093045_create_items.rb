899997fd8db70ac77eb1fd3b253f35b85aef0334
hojokaban <hojokaban@gmail.com>
2020-05-14 09:38:21 +0000

    Add null_false to columns

--- db/migrate/20200514093045_create_items.rb
+++ db/migrate/20200514093045_create_items.rb
@@ -1,12 +1,12 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.references :genre, foreign_key: true
-      t.string :name
-      t.text :explanation
-      t.integer :price
-      t.boolean :is_saled
-      t.string :item_image_id
+      t.references :genre, foreign_key: true, null: false
+      t.string :name, null: false
+      t.text :explanation, null: false
+      t.integer :price, null: false
+      t.boolean :is_saled, null: false
+      t.string :item_image_id, null: false
 
       t.timestamps
     end
--- db/migrate/20200514093456_create_orders.rb
+++ db/migrate/20200514093456_create_orders.rb
@@ -1,14 +1,14 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.references :member, foreign_key: true
-      t.integer :order_status
-      t.string :postal_code
-      t.string :address
-      t.string :name
-      t.integer :postage
-      t.integer :payment_method
-      t.integer :billing_amount
+      t.references :member, foreign_key: true, null: false
+      t.integer :order_status, null: false
+      t.string :postal_code, null: false
+      t.string :address, null: false
+      t.string :name, null: false
+      t.integer :postage, null: false
+      t.integer :payment_method, null: false
+      t.integer :billing_amount, null: false
 
       t.timestamps
     end
