54cbc98a2b1a168c36f5c3c4690bf8579b40dec0
hojokaban <hojokaban@gmail.com>
2020-05-15 00:45:34 +0000

    Add validations
    バリデーションをOrderのカラムに追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,11 @@
 class Order < ApplicationRecord
+  has_many :order_items
   belongs_to :member
+
+  validates :postal_code, presence: true, length: 7
+  validates :address, presence: true, length: { maximum: 255 }
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :postage, presence: true
+  validates :billing_amount, presence: true
+
 end
