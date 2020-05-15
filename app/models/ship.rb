4e81639ea3033f31070ea551ba5d7b8679d3a1e4
Hyouyou <mums.kh16.29@icloud.com>
2020-05-15 02:52:01 +0000

    update model

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -3,7 +3,7 @@ class Ship < ApplicationRecord
 	belongs_to :user
 
     validates　:member_id, presence: true
-	validates　:postal_code, presence: true
-	validates　:address, presence: true
-	validates　:name, presence: true
+	validates　:postal_code, presence: true, length:{is: 7}
+	validates　:address, presence: true, length:{maximum: 255}
+	validates　:name, presence: true, length:{maximum: 20}
 end
