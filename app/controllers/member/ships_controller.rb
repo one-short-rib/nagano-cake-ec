03943c147daddb30d2f3461ae38b2429c6a36eea
haruhi.m <halu0502@gmail.com>
2020-05-16 12:12:18 +0000

    Update action at ships_controller
    各アクションの詳細定義とメソッド、ストロングパラメータの定義をしました。

--- app/controllers/member/ships_controller.rb
+++ app/controllers/member/ships_controller.rb
@@ -1,7 +1,35 @@
 class Member::ShipsController < ApplicationController
+
+	before_action :set_ship, only: [:edit,:update,:destroy]
   def index
+  	@ships = current_member.ships.all
+  	@ship = current_member.ships.new
+  end
+
+  def create
+  	@ship = current_member.ships.new(ship_params)
   end
 
   def edit
+  end
+
+  def update
+  	@ship.update(ship_params)
+  	redirect_to members_ships_path
+  end
+
+  def destroy
+  	@ship.destroy
+  	redirect_to members_ships_path
+  end
+
+  private
+
+  def set_ship
+  	@ship = Ship.find(params[:id])
+	end
+
+  def ship_params
+  	params.require(:ship).permit(:postal_code,:address,:name)
   end
 end
