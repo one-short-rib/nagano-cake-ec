feb5ebe7da84adef39d8628a8e1ec504eaa2b24e
deltaloose <takeshiroyu@gmail.com>
2020-05-16 13:21:50 +0900

    fix space

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -2,8 +2,8 @@ class Ship < ApplicationRecord
 
 	belongs_to :user
 
-    validates　:member_id, presence: true
-	validates　:postal_code, presence: true
-	validates　:address, presence: true
-	validates　:name, presence: true
+    validates :member_id, presence: true
+	validates :postal_code, presence: true
+	validates :address, presence: true
+	validates :name, presence: true
 end
