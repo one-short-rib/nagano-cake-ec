a0984bef1058a4f22940557259150bc862d354f2
haruhi.m <halu0502@gmail.com>
2020-05-16 13:33:44 +0000

    Update ships_controller
    shipsのコントローラはひとまず完成

--- app/controllers/member/ships_controller.rb
+++ app/controllers/member/ships_controller.rb
@@ -3,19 +3,28 @@ class Member::ShipsController < ApplicationController
 	before_action :set_ship, only: [:edit,:update,:destroy]
   def index
   	@ships = current_member.ships.all
-  	@ship = current_member.ships.new
+  	@ship  = current_member.ships.new
   end
 
   def create
   	@ship = current_member.ships.new(ship_params)
+  	if @ship.save
+  		redirect_to members_ships_path
+  	else
+  		@ships = current_member.ships.all
+  		render :index
+  	end
   end
 
   def edit
   end
 
   def update
-  	@ship.update(ship_params)
-  	redirect_to members_ships_path
+  	if @ship.update(ship_params)
+  		redirect_to members_ships_path
+  	else
+  		render :edit
+  	end
   end
 
   def destroy
