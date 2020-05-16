ca22f6ab6b34d731e108cf4b93654a55dfdae99b
haruhi.m <halu0502@gmail.com>
2020-05-16 09:39:24 +0000

    Add view at members/edit & update action at members_controller
    登録情報の編集ページと情報を更新するupdateアクションを追加しました

--- app/controllers/member/members_controller.rb
+++ app/controllers/member/members_controller.rb
@@ -1,32 +1,34 @@
 class Member::MembersController < ApplicationController
 
-	before_action :set_member,only: [:show,:edit,:exit,:is_deleted]
+	before_action :set_member,only: [:show,:edit,:exit,:update,:is_deleted]
 
-  def show
-  end
+	def show
+	end
 
-  def edit
-  end
+	def edit
+	end
 
-  def update
-  end
+	def update
+		@member.update(member_params)
+		redirect_to members_path
+	end
 
-  def exit
-  end
+	def exit
+	end
 
-  def is_deleted
-  end
+	def is_deleted
+	end
 
-  private
-  def member_params
-  	params.require(:member).permit(:last_name,:first_name,
-  		:last_name_kana,:first_name_kana,
-  		:email,:postal_code,:address,:phone_number)
-  end
+	private
+	def member_params
+		params.require(:member).permit(:last_name,:first_name,
+			:last_name_kana,:first_name_kana,
+			:email,:postal_code,:address,:phone_number)
+	end
 
-  def set_member
+	def set_member
 		@member = current_member
-  end
+	end
 
 
 end
