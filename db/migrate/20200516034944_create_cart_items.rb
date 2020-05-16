133c390d72333c6198d9c2e676a178611b7763e6
haruhi.m <halu0502@gmail.com>
2020-05-16 05:55:49 +0000

    Fix space and indent

--- db/migrate/20200516034944_create_cart_items.rb
+++ db/migrate/20200516034944_create_cart_items.rb
@@ -2,9 +2,9 @@ class CreateCartItems < ActiveRecord::Migration[5.2]
   def change
 	    create_table :cart_items do |t|
 
-		t.integer :member_id, :null => false
-		t.integer :item_id, :null => false
-		t.integer :amount, :null => false
+			t.integer :member_id, :null => false
+			t.integer :item_id, :null => false
+			t.integer :amount, :null => false
 
   		t.timestamps
     end
