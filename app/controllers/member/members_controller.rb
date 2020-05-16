4fc4f955d63f55f9e077f06278046acfe8363548
haruhi.m <halu0502@gmail.com>
2020-05-16 08:12:25 +0000

    Add member_params at members.controller

--- app/controllers/member/members_controller.rb
+++ app/controllers/member/members_controller.rb
@@ -5,6 +5,22 @@ class Member::MembersController < ApplicationController
   def edit
   end
 
+  def update
+  end
+
   def exit
   end
+
+  def is_deleted
+  end
+
+  private
+  def member_params
+  	params.require(:member).permit(:last_name,:first_name,
+  		:last_name_kana,:first_name_kana,
+  		:email,:postal_code,:address,:phone_number)
+  end
+
+
+
 end
