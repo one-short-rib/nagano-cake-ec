dc4343d9443d81ad252efb27abfd7ae73b66bc83
hojokaban <hojokaban@gmail.com>
2020-05-17 06:26:57 +0000

    Add home integration test

--- test/controllers/member/ships_controller_test.rb
+++ test/controllers/member/ships_controller_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
 class Member::ShipsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get member_ships_index_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get member_ships_edit_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get member_ships_index_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get edit" do
+  #   get member_ships_edit_url
+  #   assert_response :success
+  # end
 
 end
--- test/integration/home_test.rb
+++ test/integration/home_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class HomeTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+    test "top view" do
+      get root_path
+      assert_select "p", "ようこそ、NaganoCakeへ！
+          このサイトは、ケーキ販売のECサイトになります。
+          会員でない方も商品の閲覧はできますが、
+          購入には会員登録が必要になります。"
+      assert_select "h2", "オススメ商品"
+    end
+
 end
