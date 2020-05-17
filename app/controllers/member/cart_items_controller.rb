d17abae4681a981f268d6de36afdd704f891095c
haruhi.m <halu0502@gmail.com>
2020-05-17 05:43:29 +0000

    Update create action,case having the one
    カートに追加した商品が既にカートにあった場合の処理を追加。updateアクションの微修正。

--- app/controllers/member/cart_items_controller.rb
+++ app/controllers/member/cart_items_controller.rb
@@ -5,7 +5,12 @@ class Member::CartItemsController < ApplicationController
   end
 
   def create
+  	@cart_items = current_member.cart_items.all
   	@cart_item = current_member.cart_items.new(cart_item_params)
+  	# もしカートに同じ商品が存在する場合、古いものを消して新しいものを保存する
+  	if @cart_items.find_by(item_id: @cart_item.item_id) = @existing_cart_item
+			@existing_cart_item.destroy
+  	end
   	if @cart_item.save
   		redirect_to members_cart_items_path
   	else
@@ -17,9 +22,10 @@ class Member::CartItemsController < ApplicationController
   	@cart_item = CartItem.find(params[:id])
   	if params[:amount] == 0
   		@cart_item.destroy
-		end
-		@cart_item.update(cart_item_params)
-  	redirect_to members_cart_items_path
+		else
+			@cart_item.update(cart_item_params)
+  		redirect_to members_cart_items_path
+  	end
   end
 
   def destroy
