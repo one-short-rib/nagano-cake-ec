3601d356686744dee86e8585ef0643d85bc3239d
haruhi.m <halu0502@gmail.com>
2020-05-17 06:24:04 +0000

    Fix action create and update

--- app/controllers/member/cart_items_controller.rb
+++ app/controllers/member/cart_items_controller.rb
@@ -8,7 +8,8 @@ class Member::CartItemsController < ApplicationController
   	@cart_items = current_member.cart_items.all
   	@cart_item = current_member.cart_items.new(cart_item_params)
   	# もしカートに同じ商品が存在する場合、古いものを消して新しいものを保存する
-  	if @cart_items.find_by(item_id: @cart_item.item_id) = @existing_cart_item
+    @existing_cart_item =  @cart_items.find_by(item_id: @cart_item.item_id)
+  	if @existing_cart_item.present?
 			@existing_cart_item.destroy
   	end
   	if @cart_item.save
@@ -20,12 +21,12 @@ class Member::CartItemsController < ApplicationController
 
   def update
   	@cart_item = CartItem.find(params[:id])
-  	if params[:amount] == 0
+    if params[:cart_item][:amount] == '0'
   		@cart_item.destroy
-		else
-			@cart_item.update(cart_item_params)
-  		redirect_to members_cart_items_path
-  	end
+    else
+      @cart_item.update(cart_item_params)
+    end
+    redirect_to members_cart_items_path
   end
 
   def destroy
