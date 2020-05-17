bb03b3118f3a0e1f0a932f2bb8e1e770d1149d79
deltaloose <takeshiroyu@gmail.com>
2020-05-17 20:12:44 +0900

    add genre

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -3,10 +3,17 @@ class Admin::MembersController < ApplicationController
 		@members = Member.all
 	end
 	def show
+		@member = Member.find(params[:id])
 	end
 	def edit
 		@member = Member.find(params[:id])
 	end
 	def update
+		@member = Member.find(params[:id])
+		if @member.update(member.params)
+			redirect_to #向かうページ
+		else
+			render #エラー作るならそのページ
+		end
 	end
 end
