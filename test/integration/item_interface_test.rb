6d8a0de248bf651171d3cc0a30d0f8c0d0848cf0
hojokaban <hojokaban@gmail.com>
2020-05-17 03:46:20 +0000

    Add show view

--- test/integration/item_interface_test.rb
+++ test/integration/item_interface_test.rb
@@ -2,9 +2,17 @@ require 'test_helper'
 
 class ItemInterfaceTest < ActionDispatch::IntegrationTest
 
+  # def setup
+  #   @item = items(:item1)
+  # end
+
   test "item idex view" do
     get items_path
     assert_select "h1", "商品一覧"
   end
-  
+
+  test "item show view" do
+    # get item_path(@item)
+  end
+
 end
