9cb461609810718bd6415135b3985801cb260878
Hyouyou <mums.kh16.29@icloud.com>
2020-05-19 03:01:38 +0000

    fixed-issues2

--- test/controllers/admin/order_items_controller_test 2.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class Admin::OrderItemsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get admin_order_items_show_url
-    assert_response :success
-  end
-
-end
