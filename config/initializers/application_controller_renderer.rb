7d0d47bb7f617fc437bf7d005ef0168499b1669f
haruhi.m <halu0502@gmail.com>
2020-05-19 02:39:29 +0000

    Add refile_secret_key
    Refileのsecret_keyを所定のファイルに記述しました。

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'adf478b1d8bbf0441dc3052da6e814bca2dfa702ce68fa5b41ec29464c3f245432dda2579abf1612757e31a26bbea5187d927cff487c7a4a8bcc114c1053caa9'
