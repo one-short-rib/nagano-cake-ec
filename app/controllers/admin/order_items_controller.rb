1cc5c16fc51343a88dcebb59bf63d67a2640d7eb
Hyouyou <mums.kh16.29@icloud.com>
2020-05-19 10:22:17 +0000

    update-controller

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -2,5 +2,8 @@ class Admin::OrderItemsController < ApplicationController
 
 	#製作ステータスの変更
   def update
+  	order_item = OrderItem.find(params[:id])
+  	order_item.update
+  	redirect_back(fallback_location: admin_top_path)
   end
 end
