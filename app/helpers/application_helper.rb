bc9b16ce2f577ee758edbcac41f29f0d971435e5
haruhi.m <halu0502@gmail.com>
2020-05-20 05:44:36 +0000

    Clean helper method
    金額計算のメソッドを一箇所にまとめ,リファクタリング。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,28 @@ module ApplicationHelper
 		member.last_name_kana+member.first_name_kana
 	end
 
+	def tax_include(price)
+		(price*1.1).floor
+	end
+
+	def subtotal_price(item)
+		tax_include(item.item.price)*item.amount
+	end
+
+	def cart_total_price
+		cart_items = current_member.cart_items.all
+		total_price = 0
+		cart_items.each do |cart_item|
+			total_price += subtotal_price(cart_item)
+		end
+		return total_price
+	end
+
+	def total_price(order)
+		cart_items = order.member.cart_items
+		total_price = 0
+		cart_items.each {|cart_item| total_price += subtotal_price(cart_item)}
+		return total_price
+	end
+
 end
--- app/helpers/member/cart_items_helper.rb
+++ app/helpers/member/cart_items_helper.rb
@@ -1,20 +1,2 @@
 module Member::CartItemsHelper
-
-	def tax_include(price)
-		(price*1.1).floor
-	end
-
-	def subtotal_price(cart_item)
-		tax_include(cart_item.item.price)*cart_item.amount
-	end
-
-	def total_price
-		cart_items = current_member.cart_items.all
-		total_price = 0
-		cart_items.each do |cart_item|
-			total_price += subtotal_price(cart_item)
-		end
-		return total_price
-	end
-
 end
