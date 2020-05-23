375772126fb92bb8d1da805ea988f4339e21cc61
deltaloose <takeshiroyu@gmail.com>
2020-05-23 18:57:31 +0900

    add-success_massage

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -5,24 +5,24 @@ class Admin::GenresController < ApplicationController
 	end
 	def create
 		@genre = Genre.new(genre_params)
-#		if 
-		@genre.save
-#			flash[:success] = ''
-		redirect_to admin_genres_path
-#		else
-#		end
+		if @genre.save
+			flash[:success] = 'Genre was successfully create'
+			redirect_to admin_genres_path
+		else
+			render :index
+		end
 	end
 	def edit
 		@genre = Genre.find(params[:id])
 	end
 	def update
 		@genre = Genre.find(params[:id])
-#		if 
-		@genre.update(genre_params)
-#			flash[:notice] = ''
-      	redirect_to admin_genres_path
-#       else
-#       end
+		if @genre.update(genre_params)
+			flash[:notice] = 'You have updated Genre successfully.'
+    	redirect_to admin_genres_path
+    else
+     	render :edit
+    end
 	end
 	private
 	def genre_params
